a764c9fcdd223c2fe7cde0624c0c4c0da528d6ac
Sungyoon Jung <30921903+greemsam@users.noreply.github.com>
2021-01-11 00:20:27 +0900

    Add files via upload

--- js/number_up_down.js
+++ js/number_up_down.js
@@ -1,14 +1,47 @@
 'use strict';
 
+var gameState = {
+  selectCharacter:true,
+  gameExecute:false,
+  deathCount:5,
+  answerRange:{
+    min:1,
+    max:100
+  }
+}
+var gameMode = {
+  modeName:'',
+}
+var gameMessages = {
+  elsa:{
+    start:"엘붕이 안녕! <br> 나랑 숫자 업앤다운 할래?",
+    win:"<span>헉...</span><br>마음을 꿰뚫어보다니 소오름...<br>",
+    lost:"<span>바보!! 못 맞추겠지?</span><br>어차피 기대도 안 했어.<br>"
+  },
+  anna:{
+    start:"안붕이 안녕! <br> 나랑 숫자 업앤다운 할래?",
+    win:"<span>헉...</span><br>내 마음을 꿰뚫어보다니 소오름...<br>",
+    lost:"<span>바보!! 못 맞추겠지?</span><br>어차피 기대도 안 했어.<br>"
+  }
+}
+var statePictures = {
+  elsa:{
+    default:'img/default_elsa.jpg',
+    win:'img/win_elsa.jpg',
+    lost:'img/lost_elsa.jpg',
+  },
+  anna:{
+    default:'img/default_anna.jpg',
+    win:'img/win_anna.jpg',
+    lost:'img/lost_anna.jpg',
+  }
+}
+var messagesByMode={};
+var picturesByMode={};
 var randomNum;
-var deathCount = 0;
-var gameState = false;
-var selectCharacter = true;
-var gameMode = '';
 var picture = document.querySelectorAll(".picture");
 var elsaPicture = document.getElementById("elsa-pic");
 var annaPicture = document.getElementById("anna-pic");
-var gameInterface = document.getElementById("game-interface");
 var numInputInterface = document.getElementById("number-input-interface");
 var input = document.getElementById("input-num");
 var gameStartBtn = document.getElementById("game-start-btn");
@@ -16,127 +49,134 @@ var submitNum = document.getElementById("submit-num");
 var printInfo = document.getElementById("print-info");
 var retry = document.getElementById("retry");
 
-elsaPicture.addEventListener('click', setElsaMode)
-annaPicture.addEventListener('click', setAnnaMode)
+init();
+
 gameStartBtn.addEventListener('click', gameStart);
 submitNum.addEventListener('click', playGame);
+
 function executeGameByEnter(){
   if (window.event.keyCode == 13) {
     playGame();
   }
 }
 function init(){
-  gameMode = '';
-  selectCharacter = true;
-  gameState = false;
-  deathCount = 0;
+  initGameState();
+  initSelectPic();
+  initButtons();
   hideRetry();
-  annaPicture.style.display = "block";
-  elsaPicture.style.display = "block";
-  annaPicture.style.float = "right";
-  elsaPicture.style.float = "left";
   printInfo.innerHTML = "좋아하는 분을 선택해주세요.";
 }
-
-function greeting(mode){
-  if(selectCharacter === true){
-    switch(mode){
-      case '엘사' : 
-        printInfo.innerHTML = "엘붕이 안녕! <br> 나랑 숫자 업앤다운 할래?"
-        break;
-      case '안나' : 
-        printInfo.innerHTML = "안붕이 안녕! <br> 나랑 숫자 업앤다운 할래?"
-        break;
-    }
-  }
+function initGameState(){
+  gameState.selectCharacter = true;
+  gameState.gameExecute = false;
+  gameState.deathCount = 5;
 }
-function showGame(){
-  gameInterface.classList.replace('off', 'on');
+function initSelectPic(){
+  picture.forEach(function(item){
+    item.classList.remove('selected');
+    item.classList.remove('not-selected');
+    item.addEventListener('click', setGameMode);
+  });
+  elsaPicture.setAttribute('src', 'img/default_elsa.jpg');
+  annaPicture.setAttribute('src', 'img/default_anna.jpg');
 }
-function hideGame(){
-  gameInterface.classList.replace('on', 'off');
+function initButtons(){
+  numInputInterface.style.display = "none";
+  gameStartBtn.style.display = "none";
 }
 function showInput(){
-  numInputInterface.classList.replace('off', 'on');    
+  numInputInterface.style.display = "block";    
 }
 function hideNumInput(){
-  numInputInterface.classList.replace('on', 'off');
+  numInputInterface.style.display = "none";
 }
-
-function setPictureCenter(){
-  picture.forEach(function(item){
-    item.style.float = "none";
-  })
-}
-function setElsaMode(){
-  gameMode = "엘사";
-  greeting(gameMode);
-  selectCharacter = false;
-  annaPicture.style.display = "none";
-  setPictureCenter();
-  showGame();
-  console.log(gameMode);
+function setGameMode(e){
+  gameState.selectCharacter = false;
   gameStartBtn.style.display = "block";
+  switch(e.target){
+    case elsaPicture : 
+      setElsaMode('elsa');
+      break;
+    case annaPicture : 
+      setAnnaMode('anna');
+      break;
+  }
 }
-function setAnnaMode(){
-  gameMode = "안나";
-  greeting(gameMode);
-  selectCharacter = false;
-  elsaPicture.style.display = "none";
-  setPictureCenter();
-  showGame();
-  console.log(gameMode);
-  gameStartBtn.style.display = "block";
+function setElsaMode(mode){
+  gameMode.modeName = mode;
+  printInfo.innerHTML = gameMessages.elsa.start;
+  annaPicture.classList.add('not-selected');
+  elsaPicture.classList.add('selected');
+  messagesByMode = gameMessages.elsa;
+  picturesByMode = statePictures.elsa; 
+}
+function setAnnaMode(mode){
+  gameMode.modeName = mode;
+  printInfo.innerHTML = gameMessages.anna.start;
+  elsaPicture.classList.add('not-selected');
+  annaPicture.classList.add('selected');
+  messagesByMode = gameMessages.anna;
+  picturesByMode = statePictures.anna; 
 } 
 function gameStart(){
-  gameStartBtn.style.display = "none";
   showInput();
-  gameState = true;
-  deathCount = 5;
-  randomNum = generateRandomNum(1, 100);
-  printInfo.innerHTML = "1 ~ 100 사이의 숫자만 입력해.";
+  generateAnswer(gameState.answerRange.min, gameState.answerRange.max);
+  gameState.gameExecute = true;
+  gameStartBtn.style.display = "none";
+  printInfo.innerHTML = gameState.answerRange.min+" ~ "+ gameState.answerRange.max+" 사이의 숫자만 입력해.";
   console.log("정답: "+ randomNum); // 정답 출력
 }
-function generateRandomNum(min, max){
-  return Math.floor(Math.random() * (max - min)) + min;
+function generateAnswer(min, max){
+  randomNum = Math.floor(Math.random() * (max - min)) + min;
 }
-
 function playGame(){
-  if(gameState === true){
+  if(gameState.gameExecute === true){
     var inputValueToNum = Number(input.value);
-    printGameResult(inputValueToNum, deathCount);
+    printGameResult(inputValueToNum, gameState.deathCount);
     checkTry(inputValueToNum);
     input.value = '';
   }
 }
 function printGameResult(val, count){
   if(randomNum === val && count>=0){
-    gameState = false;
-    printInfo.innerHTML = "<span>헉!</span><br> 내 마음을 꿰뚫어보다니 진짜 소오름...<br>정답: "+val;
-    hideNumInput();
-    showRetry();
+    youWin(val);
   }
   else if(randomNum > val && 100 >= val && count>=0){
-    printInfo.innerHTML = '<span>'+val+' 이면 업이야!</span><br>남은 횟수: '+count;
+    printInfo.innerHTML = '<span>'+val+'? 업이야!</span><br>남은 횟수: '+count;
   }
   else if(randomNum < val && 100 >= val && count>=0){
-    printInfo.innerHTML = '<span>'+val+' 이면 다운! 더 낮아!</span><br>남은 횟수: '+count;
+    printInfo.innerHTML = '<span>'+val+'? 다운! 더 낮아!</span><br>남은 횟수: '+count;
   }
   else if(randomNum !== val && count<=0){
     count = 0;
-    gameState = false;
-    printInfo.innerHTML = "바보!! 못 맞추겠지? 메롱이다!! <br> 정답은 "+randomNum+"이었지롱!";
-    hideNumInput();
-    showRetry();
+    youLost();
   }
 }
 function checkTry(inputVal){
   if (inputVal>0 && inputVal<=100){
-    deathCount--;
+    gameState.deathCount--;
   }
   else{
     printInfo.innerHTML = '응? 제대로 입력한 거 맞아? <br> 1~100 사이 숫자만 입력해줘.';
   }
+}
+function youWin(val){
+  gameState.gameExecute = false;
+  printInfo.innerHTML = messagesByMode.win+"정답: "+val;
+  picture.forEach(function(item){
+   item.setAttribute('src', picturesByMode.win);   
+  })  
+  hideNumInput();
+  showRetry();
+}
+function youLost(){
+  gameState.gameExecute = false;
+  printInfo.innerHTML = messagesByMode.lost+"정답: "+randomNum;
+  picture.forEach(function(item){
+    item.setAttribute('src', picturesByMode.lost);   
+   })  
+  hideNumInput();
+  showRetry();
 }
 function showRetry(){
   retry.style.display = "block";
